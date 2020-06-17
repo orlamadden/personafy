de4ffe0811238a7d506a7d277eeeeedb9bc70a41
Orla Madden <omadden2@gmail.com>
2020-06-17 06:48:22 +0000

    set up inital routes

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return '<h1>Home Page!</h1>'
+
+@app.route('/examples')
+def examples():
+    return '<h1>Example Personas</h1>'
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
