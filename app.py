93defe6a51c5efd68bbf1fe764f9b718044c765c
Orla Madden <omadden2@gmail.com>
2020-06-18 06:25:22 +0000

    setting up database connection

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import os
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["secret_key"] = os.environ.get('SECRET_KEY')
+
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
