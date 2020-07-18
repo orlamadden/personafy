db9b9838d9fe3fab521660e23eb718729a871911
Orla Madden <omadden2@gmail.com>
2020-07-18 13:19:35 +0000

    added more info to deployment section

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask, render_template, url_for, request, redirect, session, flash
 from flask_pymongo import PyMongo
 from datetime import datetime
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from os import path
 if path.exists("env.py"):
     import env
+
+from flask import Flask, render_template, url_for, request, redirect, session, flash
 
 app = Flask(__name__)
 
@@ -41,7 +41,7 @@ def public_personas():
     registered users to Personafy.
     """
     personas = db.persona.find()
-    
+
     occupation = [occ for occ in mongo.db.occupation.find({}, {"occupation_title": 1})]
 
     industry = [ind for ind in mongo.db.industry.find({}, {"industry_title": 1})]
@@ -58,9 +58,9 @@ def view_persona(persona_id):
     a registered user.
     """
     the_persona = db.persona.find_one({"_id": ObjectId(persona_id)})
-    
+
     occupation_name = db.occupation.find_one({"_id": ObjectId(the_persona.get("occupation_title"))})
-    
+
     industry_name = db.industry.find_one({"_id": ObjectId(the_persona.get("industry_title"))})
 
     return render_template('persona-card.html',
@@ -78,7 +78,7 @@ def add_persona():
 def insert_persona():
     """
     Create a user persona.
-    
+
     Inserts all information from the form on the
     'Add Persona' page into the database.
     """
@@ -86,15 +86,15 @@ def insert_persona():
 
     industry_title = request.form.get('industry')
     industry_id = db.industry.find_one({"industry_title": industry_title})["_id"]
-    
+
     occupation_title = request.form.get('occupation')
     occupation_id = db.occupation.find_one({"occupation_title": occupation_title})["_id"]
 
     dateAndTime = datetime.now()
-    
+
     # takes public value from the form and sets it to true
     public = True if request.form.get('option') == 'public' else False
-    
+
     if request.method == 'POST':
         persona.insert_one({
             'name': request.form.get('fname'),
@@ -111,18 +111,18 @@ def insert_persona():
         })
 
         flash("Your persona has successfully been created!")
-    
+
         return redirect(url_for('my_personas'))
     return render_template('add-persona.html', persona=persona, occupation=occupation, industry=industry)
 
 @app.route('/edit_persona/<persona_id>')
 def edit_persona(persona_id):
     the_persona = db.persona.find_one({'_id': ObjectId(persona_id)})
-    
+
     occupation_name = db.occupation.find_one({"_id": ObjectId(the_persona.get("occupation_title"))})
-    
+
     industry_name = db.industry.find_one({"_id": ObjectId(the_persona.get("industry_title"))})
-    
+
     return render_template('edit-persona.html', persona=the_persona, occupation=db.occupation.find(), industry=db.industry.find(), occupation_title=occupation_name, industry_title=industry_name)
 
 @app.route('/update_persona/<persona_id>', methods=['GET', 'POST'])
@@ -131,7 +131,7 @@ def update_persona(persona_id):
 
     industry_title = request.form.get('industry')
     industry_id = db.industry.find_one({"industry_title": industry_title})["_id"]
-    
+
     occupation_title = request.form.get('occupation')
     occupation_id = db.occupation.find_one({"occupation_title": occupation_title})["_id"]
 
@@ -154,7 +154,7 @@ def update_persona(persona_id):
     })
 
     flash("You have updated your persona.")
-    
+
     return redirect(url_for('my_personas'))
 
 @app.route('/delete_persona/<persona_id>')
@@ -162,14 +162,14 @@ def delete_persona(persona_id):
     persona = db.persona
     persona.remove({'_id': ObjectId(persona_id)})
     flash("Your persona has successfully been deleted.")
-    
+
     return redirect(url_for('my_personas'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """
     Register a user.
-    
+
     Checks the database if a username already exists, creates
     the user if the username does not exist already.
     """
@@ -189,7 +189,7 @@ def register():
         if username_exists is None:
             user.insert_one(
                 {
-                    'name' : new_username, 
+                    'name' : new_username,
                     'password': generate_password_hash(password)
                 })
             session['username'] = new_username
@@ -199,7 +199,7 @@ def register():
         else:
             flash(f'Uh oh, that username already exists. Please try another username.', 'warning')
             return redirect(url_for('register'))
-      
+
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -213,24 +213,24 @@ def login():
         username = request.form.get('reg_username')
         password = request.form.get('user_password')
         reg_user = registered_user(username)
-        
+
         # checks if username is registered is true
         if reg_user:
             """
             If registered user is true, check for hashed password
-            against the registered username and flash welcome back 
+            against the registered username and flash welcome back
             message when user successfully logs in.
             """
             if check_password_hash(reg_user["password"], password):
                 flash(f"Welcome back {username}!", "success")
                 session['username'] = username
                 return redirect(url_for('index', username=session["username"]))
-                
+
             else:
                 # Flash message on incorrect user login
                 flash("There was a problem logging in. Check your username and password combination and please try again.")
                 return redirect(url_for('login'))
-        
+
         else:
             # Flash message when username does not exist
             flash(f"The username {username} does not exist. Please create an account.")
@@ -241,7 +241,7 @@ def login():
 @app.route('/logout')
 def logout():
     """
-    Logs out the user by clearing the session cache and redirects 
+    Logs out the user by clearing the session cache and redirects
     user with flash message to index.
     """
     session.clear()
@@ -252,11 +252,11 @@ def logout():
 def my_personas():
     """
     Registered user's personas.
-    
+
     A list of all the personas created by a particular user.
     """
     personas = db.persona.find()
-    
+
     occupation = [occ for occ in mongo.db.occupation.find({}, {"occupation_title": 1})]
 
     industry = [ind for ind in mongo.db.industry.find({}, {"industry_title": 1})]
