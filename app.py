7fc889d94a3216326da9cab6621f579f9665d638
Orla Madden <omadden2@gmail.com>
2020-07-20 17:06:41 +0000

    set debug to False

--- app.py
+++ app.py
@@ -334,4 +334,4 @@ def hot_tips():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
